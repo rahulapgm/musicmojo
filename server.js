90103b6f8a3e9b84c741eb7ca9c85b2472b89ef8
rahulapgm <rahulapgm@outlook.com>
2017-05-28 14:17:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const app            = express();
 
 // server.js
 const port = 8100;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 require('./note_routes')(app, {});
 require('./index')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
+app.listen(server_ip_address,server_port, () => {
+  console.log('We are live on ' + server_port);
 });
